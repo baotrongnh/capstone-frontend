9db8f1b24d685ca333d943a6821ae00bf1ecc8e6
Bao Trong <nhbaotrong@gmail.com>
2026-01-31 01:28:02 +0700

    feat: example code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import { Button } from "antd";
 import { useTranslations } from "next-intl";
 
 export default function Home() {
+  //i18n
   const t = useTranslations('HomePage');
   return (
     <div>
       <main>
+        {/* Example i18n */}
         <h1>{t('title')}</h1>
-        <Button>asd</Button>
+        <Button>Test</Button>
       </main>
     </div>
   )
