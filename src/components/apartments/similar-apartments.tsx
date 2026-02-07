efc29d426a33289a4b81ee8bed760a74996e5314
Bao Trong <nhbaotrong@gmail.com>
2026-02-07 16:09:09 +0700

    fix (similar-apartments): ui

--- src/components/apartments/similar-apartments.tsx
+++ src/components/apartments/similar-apartments.tsx
@@ -73,12 +73,3 @@ export default function SimilarApartments() {
           </div>
      )
 }
-
-const contentStyle: React.CSSProperties = {
-     margin: 0,
-     height: '160px',
-     color: '#fff',
-     lineHeight: '160px',
-     textAlign: 'center',
-     background: '#364d79',
-};
