ed1f411708d6baf6cd19b5bf673160cda64f5489
Aries <lehuy25032004@gmail.com>
2026-03-12 20:32:35 +0700

    feat(profile): add base UI bill and my-properties page

--- src/app/profile/components/index.ts
+++ src/app/profile/components/index.ts
@@ -4,3 +4,5 @@ export { default as ProfileSidebar } from './profile-sidebar';
 export { default as AccountInformation } from './account-information';
 export { default as MyApartment } from './my-apartment';
 export { default as PaymentHistory } from './payment-history';
+export { default as Bills } from './bills';
+export { default as MyProperties } from './my-properties';
