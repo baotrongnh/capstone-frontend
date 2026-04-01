388cb8f9ee882bc811d63acd94a26b5a02406519
Bao Trong <nhbaotrong@gmail.com>
2026-04-01 17:04:36 +0700

    fix: bug ui on filter

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,18 @@ const nextConfig: NextConfig = {
                 port: '',
                 pathname: '/**',
             },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'ftlxjzrbxwkmasktlvix.supabase.co',
+                port: '',
+                pathname: '/**',
+            },
         ],
         domains: ["example.com"],
     },
